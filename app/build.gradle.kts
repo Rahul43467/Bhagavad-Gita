5a8c56557dc2345f4d53641d99a6682f05edd5fc
rahulmishra <mishra05rahul1992@gmail.com>
2024-03-29 16:08:05 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,14 +49,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
 
+
+
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$2.7.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    annotationProcessor("androidx.room:room-compiler:$2.6.1")
+
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
